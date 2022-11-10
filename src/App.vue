b634a7a61617ab4f7671fec22cd54229626ba1fe
Chinnawat003 <Chinnawat1926@gmail.com>
2022-11-10 17:37:21 +0700

    Up1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  
+  <div class="text-lg bg-red-500 text-white  flex items-center p-4 shadow">
+    <router-link to="/" class="flex items-center h-10 px-4 ">Home</router-link> |
+    <router-link to="/about" class="flex items-center  h-10 px-4 ">About</router-link> |
+    <router-link to="/addpost" class="flex items-center  h-10 px-4 ">Addpost</router-link>
+  </div>
+
+  <RouterView />
+</template>
+
+<script>
+  export default{
+    name:"Vue3-Blog"
+  }
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-for="text in 3" :key="text.id" class="flex bg-white w-[650] shadow mx-auto mt-2">
+      <div class="p-2 flex-shrink-0">
+          <img class="shadow" height="250" width="250" src="https://picsum.photos/536/354" alt="img1">
+      </div>
+      <div class="ml-2 p-2">
+        <div class="font-bold text-lg text-purple-600">fsdfsdfsdf</div>
+        <div class="text-sm">
+          กรุงเทพมหานคร เป็นเมืองหลวงและนครที่มีประชากรมากที่สุดของประเทศไทย
+          เป็นศูนย์กลางการปกครอง การศึกษา การคมนาคมขนส่ง การเงินการธนาคาร
+          การพาณิชย์ การสื่อสาร และความเจริญของประเทศ
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HomePage",
+  };
+  </script>
+  <style></style>
